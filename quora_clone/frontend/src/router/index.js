692a9ecf2c206b190b5c8296eaf84a45509b50fa
Jordan Engstrom <jordan.engstrom@gmail.com>
2020-12-15 16:12:52 -0700

    started customizing frontend. now enforcing csrf token auth

--- quora_clone/frontend/src/router/index.js
+++ quora_clone/frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Example from "@/views/Example.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
@@ -13,17 +15,18 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: About
+  },
+  {
+    path: "/example",
+    name: "Example",
+    component: Example
   }
 ];
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
   routes
 });
 
