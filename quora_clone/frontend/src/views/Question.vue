d40fe413109b031a027ef28f646d623c307afbe9
Jordan Engstrom <jordan.engstrom@gmail.com>
2020-12-15 22:49:28 -0700

    added answer pagination

--- quora_clone/frontend/src/views/Question.vue
+++ quora_clone/frontend/src/views/Question.vue
@@ -44,6 +44,16 @@
         :answer="answer"
         :key="index"
       />
+      <div class="my-4">
+        <p v-show="loadingAnswers">...loading...</p>
+        <button
+          v-show="next"
+          @click="getQuestionAnswers"
+          class="btn btn-sm btn-outline-success"
+        >
+          Load More
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -70,7 +80,9 @@ export default {
       newAnswerBody: null,
       error: null,
       userHasAnswered: false,
-      showForm: false
+      showForm: false,
+      next: null,
+      loadingAnswers: false
     };
   },
   methods: {
@@ -87,8 +99,18 @@ export default {
     },
     getQuestionAnswers() {
       let endpoint = `/api/questions/${this.slug}/answers/`;
+      if (this.next) {
+        endpoint = this.next;
+      }
+      this.loadingAnswers = true;
       apiService(endpoint).then(data => {
-        this.answers = data.results;
+        this.answers.push(...data.results);
+        this.loadingAnswers = false;
+        if (data.next) {
+          this.next = data.next;
+        } else {
+          this.next = null;
+        }
       });
     },
     onSubmit() {
