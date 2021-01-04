809dc2e21a059654337ead58dee23b3e10e26e39
Jordan Engstrom <jordan.engstrom@gmail.com>
2021-01-04 11:32:11 -0700

    added mail_urls to users.urls.py

--- quora_clone/users/api/urls.py
+++ quora_clone/users/api/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
+from django_email_verification import urls as mail_urls
 from users.api.views import CurrentUserAPIView
 
-
 urlpatterns = [
-    path('user/', CurrentUserAPIView.as_view(), name='current-user')
+    path('user/', CurrentUserAPIView.as_view(), name='current-user'),
+    path('email/', include(mail_urls)),
 ]
--- quora_clone/users/views.py
+++ quora_clone/users/views.py
@@ -6,4 +6,5 @@ from django_email_verification import sendConfirm
 def myCreateView(request):
     user = get_user_model().objects.create(username=request.username, password=request.password1, email=request.email)
     sendConfirm(user)
+    print('made it here')
     return {'new_user_created': True}
