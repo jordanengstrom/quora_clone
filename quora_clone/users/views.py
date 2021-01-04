14a2545b33f030f9b592587196eff3fd074353a9
Jordan Engstrom <jordan.engstrom@gmail.com>
2021-01-04 08:52:39 -0700

    adding django-email-verification library

--- quora_clone/users/views.py
+++ quora_clone/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django_email_verification import sendConfirm
 
-# Create your views here.
+
+def myCreateView(request):
+    user = get_user_model().objects.create(username=request.username, password=request.password1, email=request.email)
+    sendConfirm(user)
+    return {'new_user_created': True}
